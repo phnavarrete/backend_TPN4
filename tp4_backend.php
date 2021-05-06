02d37d8eb3357da90a7ece594e63c7ebf9488205
pablo_navarrete <you@example.com>
2021-05-05 23:49:27 -0300

    terminado

--- tp4_backend.php
+++ tp4_backend.php
@@ -8,43 +8,49 @@
     <h1> EJERCICIO 1 </h1>
       <h2 style="color:BROWN">
       <?php
-      $n=1;
-      while ($n <= 100) {
-        echo "$n</p>";
-        $n++;
-      }
+        $pares1al20 = array();
+        for($cont = 0; $cont<20; $cont=$cont+2){
+          $pares1al20[$cont] = $cont;
+        }
+        for($i = 0; $i<20; $i=$i+2){
+           echo "$pares1al20[$i]<br>";
+        }
       ?>
     </h2>
 <hr>
     <h1> EJERCICIO 2 </h1>
       <h2 style="color:BLUE">
         <?php
-        $n=100;
-        while ($n <= 100 and $n > 0) {
-          echo "$n</p>";
-          $n--;
-        }
+        $arrayej2 = ["Pedro","Ana",34, 1,];
+        print_r($arrayej2);          
         ?>
     </h2>
 <hr>
     <h1> EJERCICIO 3 </h1>
       <h2 style="color:GREEN">
         <?php
-        $f=2;
-        while ($f <= 100) {
-          echo "$f</p>";
-          $f=$f+2;
-        }
+        $datospersonales = [
+          "Nombre"=>"Pedro",
+          "Apellido"=>"Torres",
+          "Dirección"=>"Av. Mayor 3703",
+          "Teléfono"=>"1122334455",
+        ]
         ?>
     </h2>
 <hr>
     <h1> EJERCICIO 4 </h1>
       <h2 style="color:RED">
         <?php
-        $f=1;
-        while ($f <= 100) {
-          echo "$f</p>";
-          $f=$f+2;
+        $ciudades=[
+          "Madrid", 
+          "Barcelona", 
+          "Londres", 
+          "New York", 
+          "Los Ángeles",
+          "Chicago",
+        ];
+        for($k = 0; $k<6; $k++){
+          echo "La ciudad con el índice $k tiene el nombre $ciudades[$k].<br>";
         }
         ?>
     </h2>
@@ -52,24 +58,27 @@
     <h1> EJERCICIO 5 </h1>
       <h2 style="color:violet">
         <?php
-        $r=0;
-        for ($i=1; $i<=20; $i++) {
-        $r=$i+$r;
-        echo "$r<br>";
+        $ciudades1=[
+          "MD", 
+          "BCL",
+          "LD", 
+          "NY", 
+          "LA",
+          "CCG",
+        ];
+        $ciudades2=[
+        "Madrid", 
+        "Barcelona",
+        "Londres", 
+        "New York", 
+        "Los Ángeles",
+        "Chicago",
+        ];
+        for($k = 0; $k<6; $k++){
+          echo "El índice de $ciudades2[$k] es $ciudades1[$k].<br>";
         }
         ?>
     </h2>
 <hr>
-    <h1> EJERCICIO 6 </h1>
-      <h2 style="color:grey">
-        <?php
-        $r=0;
-        for ($i=2; $i<=20; $i=$i+2) {
-        $r=$i+$r;
-        echo "$r<br>";
-        }
-        ?>
-      </h2>
-
   </body>
 </html>
